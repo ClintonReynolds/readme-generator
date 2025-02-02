00f59a3008135c08636c288d92303de7f6b62273
clinton reynolds <eastwood188209@gmail.com>
2025-02-02 08:49:04 -0500

    finished questions and included packages

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # readme-generator
+
+
+## Another Title
--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 // TODO: Include packages needed for this application
+import generateMarkdown from './utils/generateMarkdown.js';
+import inquirer from "inquirer";
+import colors from "colors";
+import fs from "fs";
+import Choice from 'inquirer/lib/objects/choice.js';
+import Choices from 'inquirer/lib/objects/choices.js';
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: 'input',
     name: 'title',
-    message: 'What is the title of your project'
+    message: 'What is the title of your project?' 
   },
   {
     type: 'input',
     name: 'description',
-    message: 'What is the description of your project'
+    message: 'What is the description of your project?'  
+  },
+  {
+    type: 'input',
+    name: 'instalation',
+    message: 'What do you need to install this project?'
+  },
+  {
+    type: 'input',
+    name: 'licences',
+    message: 'please select a license you would like to use',
+    Choices: ['MIT License', 'Apache License 2.0', 'BSD 3-Clause License', 'Mozilla Public License 2.0', 'GNU General Public License (GPL) v3']
   },
   
 ];
